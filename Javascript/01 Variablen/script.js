220bf8f923719047ea9160dee4f14c9bc2219d25
dorotea007 <dorotea.milic@student.htldornbirn.at>
2025-01-12 15:42:45 +0100

    finished 01

---
+++ Javascript/01 Variablen/script.js
@@ -0,0 +1,43 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+
+let result = a + b + c;
+console.log("a + b + c = " + result);
+// a + b + c
+
+result = (a + b) / c;
+console.log("(a + b) / c = " + result);
+// (a + b) / c
+
+result = c - a + b;
+console.log("c - a + b = " + result);
+// c -a + b
+
+result = a + b + c * 3;
+console.log("a + b + c * 3 = " + result);
+// a + b + c * 3
+
+result = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + result);
+
+// (a + b + c)/4
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
+a = 3;
+console.log("Die neue Variable für a = " + a);
+
+b = a + 3;
+console.log("Die neur Variable für b = " + b);
+
+c = c - 3;
+console.log("Die neue Variable für c = " + c);
