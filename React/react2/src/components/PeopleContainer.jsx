5b2adad62daf983de8b30cd2d731ab8e2e387e1a
dorotea007 <dorotea.milic@student.htldornbirn.at>
2025-05-11 16:57:22 +0200

    finished demo filter

--- React/react2/src/components/PeopleContainer.jsx
+++ React/react2/src/components/PeopleContainer.jsx
@@ -3,23 +3,44 @@ import CardPeople from './CardPeople';
 
 export default function PeopleContainer() {
   const [people, setPeople] = useState([]);
+  const [filteredPeople, setFilteredPeople] = useState([]);
 
-  useEffect(() => {
-    fetch("https://67c08172b9d02a9f224a0b60.mockapi.io/person").then
-      ((res) => res.json()).then((data) => {
+  useEffect(() => { // useState merkt sich Daten über mehrere Aufrufe hinweg
+    fetch("https://67c08172b9d02a9f224a0b60.mockapi.io/person").then(
+      (res) => res.json()).then((data) => {
         setPeople(data);
+        setFilteredPeople(data);
       })
   }, []);
 
+  const filterPeople = (filter) =>{
+    let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+    setFilteredPeople(filtered);
+  }
+
   return (
-    <div>
-      <h1>People</h1>
-      <button onClick={()=>{setcolor("orange")}}>Change Color</button>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-        {people.map(person => {
-          return <CardPeople name={person.name} imageUri={person.avatar} title={person.jobtitle}/>
+    <div className="">
+      <div className="fixed top-0 border mb- bg-white w-full h-28 p-8">
+        <input 
+          className="p-2 bg-white" 
+          type="text"
+          placeholder="Search" 
+          onChange={(el)=>{
+            console.log(el.target.value);
+            filterPeople(el.target.value);
+        }} 
+        />
+      </div>
+      
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32">
+        {filteredPeople.map(person => {
+          return <CardPeople 
+          name={person.name} 
+          imageUri={person.avatar} 
+          title={person.jobtitle}/>
         })}
       </div>
     </div>
   );
 }
+
